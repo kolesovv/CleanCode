68bb27c32208cae824e5af5fdfbb3c27de618f26
Kolesov Sergey <60474836+kolesovv@users.noreply.github.com>
2020-05-03 13:44:24 +0300

    Add files via upload

---
+++ aircompany-master/Java/src/main/java/Airport.java
@@ -0,0 +1,123 @@
+import Planes.ExperimentalPlane;
+import models.MilitaryType;
+import Planes.MilitaryPlane;
+import Planes.PassengerPlane;
+import Planes.Plane;
+
+import java.util.*;
+
+public class Airport {
+    private List<? extends Plane> planes;
+
+    public List<PassengerPlane> getPassengerPlane() {
+        List<? extends Plane> planes = this.planes;
+        List<PassengerPlane> passengerPlanes = new ArrayList<>();
+        for (Plane currentPlane : planes) {
+            if (currentPlane instanceof PassengerPlane) {
+                passengerPlanes.add((PassengerPlane) currentPlane);
+            }
+        }
+        return passengerPlanes;
+    }
+
+    public List<MilitaryPlane> getMilitaryPlanes() {
+        List<MilitaryPlane> militaryPlanes = new ArrayList<>();
+        for (Plane currentPlane : planes) {
+            if (currentPlane instanceof MilitaryPlane) {
+                militaryPlanes.add((MilitaryPlane) currentPlane);
+            }
+        }
+        return militaryPlanes;
+    }
+
+    public PassengerPlane getPassengerPlaneWithMaxPassengersCapacity() {
+        List<PassengerPlane> passengerPlanes = getPassengerPlane();
+        PassengerPlane planeWithMaxCapacity = passengerPlanes.get(0);
+        for (PassengerPlane passengerPlane : passengerPlanes) {
+            if (passengerPlane.getPassengersCapacity() > planeWithMaxCapacity.getPassengersCapacity()) {
+                planeWithMaxCapacity = passengerPlane;
+            }
+        }
+        return planeWithMaxCapacity;
+    }
+
+    public List<MilitaryPlane> getTransportMilitaryPlanes() {
+        List<MilitaryPlane> transportMilitaryPlanes = new ArrayList<>();
+        List<MilitaryPlane> militaryPlanes = getMilitaryPlanes();
+        for (MilitaryPlane plane : militaryPlanes) {
+            if (plane.getType() == MilitaryType.TRANSPORT) {
+                transportMilitaryPlanes.add(plane);
+            }
+        }
+        return transportMilitaryPlanes;
+    }
+
+    public List<MilitaryPlane> getBomberMilitaryPlanes() {
+        List<MilitaryPlane> bomberMilitaryPlanes = new ArrayList<>();
+        List<MilitaryPlane> militaryPlanes = getMilitaryPlanes();
+        for (MilitaryPlane plane : militaryPlanes) {
+            if (plane.getType() == MilitaryType.BOMBER) {
+                bomberMilitaryPlanes.add(plane);
+            }
+        }
+        return bomberMilitaryPlanes;
+    }
+
+    public List<ExperimentalPlane> getExperimentalPlanes() {
+        List<ExperimentalPlane> experimentalPlanes = new ArrayList<>();
+        for (Plane currentPlane : planes) {
+            if (currentPlane instanceof ExperimentalPlane) {
+                experimentalPlanes.add((ExperimentalPlane) currentPlane);
+            }
+        }
+        return experimentalPlanes;
+    }
+
+    public Airport sortByMaxDistance() {
+        planes.sort(new Comparator<Plane>() {
+            public int compare(Plane planeFirst, Plane planeSecond) {
+                return planeFirst.getMaxFlightDistance() - planeSecond.getMaxFlightDistance();
+            }
+        });
+        return this;
+    }
+
+    public Airport sortByMaxSpeed() {
+        planes.sort(new Comparator<Plane>() {
+            public int compare(Plane planeFirst, Plane planeSecond) {
+                return planeFirst.getMaxSpeed() - planeSecond.getMaxSpeed();
+            }
+        });
+        return this;
+    }
+
+    public Airport sortByMaxLoadCapacity() {
+        planes.sort(new Comparator<Plane>() {
+            public int compare(Plane o1, Plane o2) {
+                return o1.getMinLoadCapacity() - o2.getMinLoadCapacity();
+            }
+        });
+        return this;
+    }
+
+    public List<? extends Plane> getPlanes() {
+        return planes;
+    }
+
+    private void print(Collection<? extends Plane> collection) {
+        for (Plane plane : collection) {
+            System.out.println(plane);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "Planes=" + planes.toString() +
+                '}';
+    }
+
+    public Airport(List<? extends Plane> planes) {
+        this.planes = planes;
+    }
+}
